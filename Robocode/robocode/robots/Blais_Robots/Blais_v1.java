3f5db89baa6ea96bf549fa338c648f1229ed855d
Patrick <pwblais@icloud.com>
2016-09-21 17:11:59 -0400

    Commit

--- Robocode/robocode/robots/Blais_Robots/Blais_v1.java
+++ Robocode/robocode/robots/Blais_Robots/Blais_v1.java
@@ -12,22 +12,21 @@ package Blais_Robots;
 import robocode.HitRobotEvent;
 import robocode.Robot;
 import robocode.ScannedRobotEvent;
+//import robocode.*;
 
 import java.awt.*;
 
-
 /**
- * Walls - a sample robot by Mathew Nelson, and maintained by Flemming N. Larsen
- * <p/>
- * Moves around the outer edge with the gun facing in.
- *
- * @author Mathew A. Nelson (original)
- * @author Flemming N. Larsen (contributor)
- */
-public class Blais_v1 extends Robot {
+  Potbot: a robot by Patrick Blais
+*/
 
-	boolean peek; // Don't turn if there's a robot there
-	double moveAmount; // How much to move
+public class Blais_v1 extends Robot {
+  double previousEnergy = 100;
+  int movementDirection = 1;
+  int gunDirection = 1;
+  double randomNum;
+	boolean peek; // Don't turn if someone is there
+	double moveAmount;
 
 	/**
 	 * run: Move around the walls
@@ -36,9 +35,9 @@ public class Blais_v1 extends Robot {
 		// Set colors
 		setBodyColor(Color.black);
 		setGunColor(Color.black);
-		setRadarColor(Color.orange);
-		setBulletColor(Color.cyan);
-		setScanColor(Color.cyan);
+		setRadarColor(Color.black);
+		setBulletColor(Color.black);
+		setScanColor(Color.black);
 
 		// Initialize moveAmount to the maximum possible for this battlefield.
 		moveAmount = Math.max(getBattleFieldWidth(), getBattleFieldHeight());
@@ -84,12 +83,40 @@ public class Blais_v1 extends Robot {
 	 * onScannedRobot:  Fire!
 	 */
 	public void onScannedRobot(ScannedRobotEvent e) {
-		fire(2);
-		// Note that scan is called automatically when the robot is moving.
-		// By calling it manually here, we make sure we generate another scan event if there's a robot on the next
-		// wall, so that we do not start moving up it until it's gone.
-		if (peek) {
-			scan();
-		}
+		// fire(2);
+		// // Note that scan is called automatically when the robot is moving.
+		// // By calling it manually here, we make sure we generate another scan event if there's a robot on the next
+		// // wall, so that we do not start moving up it until it's gone.
+		// if (peek) {
+		// 	scan();
+    boolean spotted = true;
+    double changeInEnergy = previousEnergy-e.getEnergy();
+    if (changeInEnergy > 0 && changeInEnergy <=3) {
+      //THEY JUST SHOT AT ME DODGE WITH A RANDOMLY GENERATED MOVEMENT
+      //This nice little piece of code will generate a random number between 0.1 and 2
+      randomNum = 0.1 + (int)(Math.random() * 2);
+
+      while (spotted) {
+        if (randomNum > 0.1 && randomNum =< 0.3) {
+          //ADD RANDOM #1
+        } else if (randomNum > 0.3 && randomNum =< 0.5) {
+          //ADD RANDOM #2
+        } else if () {
+          //ADD RANDOM #3
+        } else if () {
+          //ADD RANDOM #4
+        } else if () {
+          //ADD RANDOM #5
+        } else {
+          //ADD RANDOM #5
+        }
+      }
+    }
+
+    double previousEnergy = e.getEnergy(); //Sets previousEnergy to be the post-shot energy
+	}
+
+  public void dodgeScript () {
+
 	}
 }
