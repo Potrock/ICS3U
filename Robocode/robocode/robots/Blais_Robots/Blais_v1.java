360d12e9565bba9d5385183465e018d819a05835
Potrock <pwblais@icloud.com>
2016-09-21 21:54:27 -0400

    V1

--- Robocode/robocode/robots/Blais_Robots/Blais_v1.java
+++ Robocode/robocode/robots/Blais_Robots/Blais_v1.java
@@ -7,11 +7,11 @@
 		Also, use Math.random with a min/max to randomize movement at times to avoid trig trackers
  */
 
- // TEST FOR GIT SYNCING
 
- //SECOND TEST FOR GIT SYNCING
+// Classpath: C:\Users\pwbl\Documents\GitHub\ICS3U\Robocode\robocode\robots\Blais_Robots\Blais_v1.java
+
+
 package Blais_Robots;
-
 
 import robocode.HitRobotEvent;
 import robocode.Robot;
@@ -81,46 +81,74 @@ public class Blais_v1 extends Robot {
 		else {
 			ahead(100);
 		}
-	}
+  }
 
 	/**
 	 * onScannedRobot:  Fire!
 	 */
 	public void onScannedRobot(ScannedRobotEvent e) {
-		// fire(2);
-		// // Note that scan is called automatically when the robot is moving.
-		// // By calling it manually here, we make sure we generate another scan event if there's a robot on the next
-		// // wall, so that we do not start moving up it until it's gone.
-		// if (peek) {
-		// 	scan();
-    boolean spotted = true;
+		fire(2);
+		if (peek) {
+    scan();
+	}
     double changeInEnergy = previousEnergy-e.getEnergy();
     if (changeInEnergy > 0 && changeInEnergy <=3) {
-      //THEY JUST SHOT AT ME DODGE WITH A RANDOMLY GENERATED MOVEMENT
+      boolean spotted = true;
+      //THEY JUST SHOT AT ME DODGE WITH A (kinda)RANDOMLY GENERATED MOVEMENT
       //This nice little piece of code will generate a random number between 0.1 and 2
       randomNum = 0.1 + (int)(Math.random() * 2);
 
       while (spotted) {
-        if (randomNum > 0.1 && randomNum =< 0.3) {
-          //ADD RANDOM #1
-        } else if (randomNum > 0.3 && randomNum =< 0.5) {
-          //ADD RANDOM #2
-        } else if () {
-          //ADD RANDOM #3
-        } else if () {
-          //ADD RANDOM #4
-        } else if () {
-          //ADD RANDOM #5
+        if (randomNum > 0.1 && randomNum <= 0.3) { //Take our random number, and turn it into a (kinda)random movement!
+          turnLeft(90);
+          ahead(100);
+          turnLeft(90);
+          ahead(100);
+          turnLeft(90);
+          ahead(100);
+          scan();
+        } else if (randomNum > 0.3 && randomNum <= 0.5) {
+          back(100);
+          ahead(100);
+          scan();
+        } else if (randomNum > 0.5 && randomNum <= 0.8) {
+          turnRight(90);
+          ahead(100);
+          turnRight(90);
+          ahead(100);
+          turnRight(90);
+          ahead(100);
+          scan();
+        } else if (randomNum > 0.8 && randomNum <= 1) {
+          ahead(100);
+          back(200);
+          scan();
+        } else if (randomNum > 1 && randomNum <= 1.4) {
+          ahead(100);
+          back(200);
+          scan();
+        } else if (randomNum > 1.4 && randomNum <= 1.7) {
+          back(300);
+          turnRight(90);
+          back(100);
+          turnRight(90);
+          back(300);
+          scan();
+        } else if (randomNum > 1.7 && randomNum <= 2) {
+          turnLeft(90);
+          ahead(50);
+          turnRight(90);
+          ahead(50);
+          scan();
         } else {
-          //ADD RANDOM #5
+          turnLeft(90);
+          ahead(100);
+          turnLeft(90);
+          ahead(100);
+          scan();
         }
       }
+      turnGunRight(360); //Sweeps scanner?
     }
-
-    double previousEnergy = e.getEnergy(); //Sets previousEnergy to be the post-shot energy
-	}
-
-  public void dodgeScript () {
-
-	}
+  }
 }
